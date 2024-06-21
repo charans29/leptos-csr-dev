9e9a903fa7af8b5b7feb8714e94696131e594ca0
charans29 <smodugula@csuchico.edu>
2024-06-20 19:56:58 -0700

    feat: inner HTML injection

--- dist/leptos-csr-dev-3a876394ce1ad768.js
+++ dist/leptos-csr-dev-3a876394ce1ad768.js
@@ -811,15 +811,15 @@ imports.wbg.__wbindgen_memory = function() {
     return addHeapObject(ret);
 };
 imports.wbg.__wbindgen_closure_wrapper97 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 32, __wbg_adapter_28);
+    const ret = makeMutClosure(arg0, arg1, 37, __wbg_adapter_28);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper1749 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 152, __wbg_adapter_31);
+imports.wbg.__wbindgen_closure_wrapper1790 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 157, __wbg_adapter_31);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper6754 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 195, __wbg_adapter_34);
+imports.wbg.__wbindgen_closure_wrapper6795 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 200, __wbg_adapter_34);
     return addHeapObject(ret);
 };
 
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,31 @@ use leptos::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <App/> })
+    leptos::mount_to_body(App)
 }
 
 #[component]
 fn app() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let _html = "<p>This is inner HTML injected.</p>";
 
     view! {
-        <button
-            on:click={move |_| {
-                set_count.update(|n| *n += 10);
-            }}
-            style="position: relative"
-            style:left=move || format!("{}px", count() + 10)
-            style:background-color=move || format!("rgb({}, {}, 205)", count(), 90)
-            style:max-width="400px"
-            style=("--columns", count)
-            class:red= move || count() % 2 == 0 
-        >
-            "Click me: "
-            {move || count()}
-        </button>
+        <div style="text-align: center" inner_html=_html />
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+            <progress
+                max="300"
+                value=move || count() / 2
+            />
+            <button
+                on:click={move |_| {
+                    set_count.update(|n| *n += 10);
+                }}          
+                style:background-color=move || format!("rgb({}, {}, 205)", count(), 90)
+                class:red= move || count() % 2 == 0 
+            >
+                "Click me: "
+                {move || count()}
+            </button>
+        </div>
     }
 }
